4194669dfd119bb0088ae986ea9e3a90d2dc2ab5
SFrasia <108404961+SFrasia@users.noreply.github.com>
2024-01-22 22:07:15 +0300

    Changes

---
+++ src/index.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", () => {
+  // your code here
+  const form = document.getElementById("create-task-form");
+  const description = document.getElementById("new-task-description");
+  const list = document.getElementById("tasks");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const delBtn = document.createElement("button");
+    delBtn.textContent = "X";
+    const val = description.value;
+    delBtn.addEventListener("click", (e) => {
+      e.target.parentNode.remove();
+    });
+    delBtn.style = "left:20px";
+    const newLi = document.createElement("li");
+    newLi.textContent = val + "  ";
+    newLi.appendChild(delBtn);
+    list.appendChild(newLi);
+    description.value = "";
+  });
+  const ulist = document.getElementById("tasks");
+});
